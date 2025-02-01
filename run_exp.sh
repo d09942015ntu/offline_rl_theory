cffce7cfc37edeb0bc51be9b60e086a20112c57e
d09942105 <d09942015@ntu.edu.tw>
2025-02-01 15:07:45 +0800

    refactor 3

--- run_exp.sh
+++ run_exp.sh
@@ -19,9 +19,9 @@
 #python3 evaluator_carpole.py --kernel=kernel_gaussian --phi=phi_array --output_dir=results/kernel_gaussian --repeat=50 --n1s=5 --n2s=6
 
 
-python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_cubic --output_dir=results/kernel_cubic_100 --repeat=50 --n1s 100
-python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_cubic --output_dir=results/kernel_cubic_200 --repeat=50 --n1s 200
-python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_quadratic --output_dir=results/kernel_quadratic_100 --repeat=50 --n1s 100
-python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_quadratic --output_dir=results/kernel_quadratic_200 --repeat=50 --n1s 200
-python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_linear --output_dir=results/kernel_linear_100 --repeat=50 --n1s 100
-python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_linear --output_dir=results/kernel_linear_200 --repeat=50 --n1s 200
+python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_cubic --output_dir=results/kernel_cubic_100 --repeat=50 --n1s 100 &
+python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_cubic --output_dir=results/kernel_cubic_200 --repeat=50 --n1s 200 &
+python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_quadratic --output_dir=results/kernel_quadratic_100 --repeat=50 --n1s 100 &
+python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_quadratic --output_dir=results/kernel_quadratic_200 --repeat=50 --n1s 200 &
+python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_linear --output_dir=results/kernel_linear_100 --repeat=50 --n1s 100 &
+python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_linear --output_dir=results/kernel_linear_200 --repeat=50 --n1s 200 &

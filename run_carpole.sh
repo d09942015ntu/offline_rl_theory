32304b2131551700ae837e80a7f5f9f0c575bf4b
d09942105 <d09942015@ntu.edu.tw>
2025-02-02 20:26:00 +0800

    update run_carpole.sh

--- run_carpole.sh
+++ run_carpole.sh
@@ -1,22 +1,4 @@
 
-#python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_linear --output_dir=results/temp_kernel_linear --repeat=1 --n1s=1 --n1s=1
-#python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_quadratic --output_dir=results/temp_kernel_quadratic --repeat=1 --n1s=1 --n1s=1
-#python3 evaluator_carpole.py --kernel=kernel_gaussian --phi=phi_array --output_dir=results/temp_kernel_gaussian --repeat=1 --n1s=1 --n1s=1
-#
-#
-#python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_linear --output_dir=results/kernel_linear --repeat=50 --n1s=5 --n1s=6
-#python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_quadratic --output_dir=results/kernel_quadratic --repeat=50 --n1s=5 --n1s=6
-#python3 evaluator_carpole.py --kernel=kernel_gaussian --phi=phi_array --output_dir=results/kernel_gaussian --repeat=50 --n1s=5 --n1s=6
-
-#python3 evaluator_fzlake.py --kernel=kernel_linear --phi=phi_tabular --output_dir=results/temp_fzlake_tabular --repeat=1 --n1s=1 --n1s=1
-#python3 evaluator_fzlake.py --kernel=kernel_gaussian --phi=phi_array --output_dir=results/temp_fzlake_gaussian --repeat=1 --n1s=1 --n1s=1
-#
-#python3 evaluator_fzlake.py --kernel=kernel_linear --phi=phi_tabular --output_dir=results/temp2_fzlake_tabular --repeat=5 --n1s=5 --n1s=6
-#python3 evaluator_fzlake.py --kernel=kernel_gaussian --phi=phi_array --output_dir=results/temp2_fzlake_gaussian --repeat=5 --n1s=5 --n1s=6
-
-#python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_linear --output_dir=results/kernel_linear --repeat=50 --n1s=5 --n1s=6
-#python3 evaluator_carpole.py --kernel=kernel_linear --phi=phi_quadratic --output_dir=results/kernel_quadratic --repeat=50 --n1s=5 --n1s=6
-#python3 evaluator_carpole.py --kernel=kernel_gaussian --phi=phi_array --output_dir=results/kernel_gaussian --repeat=50 --n1s=5 --n1s=6
 
 
 n1ss=(20 50 100 200)
